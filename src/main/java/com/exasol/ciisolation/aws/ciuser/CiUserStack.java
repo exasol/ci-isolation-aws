ae00b38a512d994ac5a2667496a8f726508f23c5
Antoni Reus <antoni.reus@exasol.com>
2025-03-04 10:10:13 +0100

    Add the possibility to create a role to the stack

--- src/main/java/com/exasol/ciisolation/aws/ciuser/CiUserStack.java
+++ src/main/java/com/exasol/ciisolation/aws/ciuser/CiUserStack.java
@@ -12,12 +12,15 @@ import software.amazon.awscdk.services.iam.*;
 import software.constructs.Construct;
 
 /**
- * This class defines a CloudFormation stack with a user for CI testing. Typically this stack is used from a project
+ * This class defines a CloudFormation stack with a user for CI testing. Typically, this stack is used from a project
  * specific CI CDK setup.
  */
 public class CiUserStack extends TaggedStack {
-    /** The output name of the CI user name */
+    /** The output name of the CI userName */
     public static final String OUTPUT_CI_USER_NAME = "ciUserName";
+    /** The output ARN of the CI role */
+    public static final String OUTPUT_CI_ROLE_ARN = "ciRoleArn";
+
     private static final String PROTECTED = "protected-";
 
     /**
@@ -31,34 +34,61 @@ public class CiUserStack extends TaggedStack {
     }
 
     private void defineResources(final CiUserStackProps props) {
-        final String ciUserName = PROTECTED + props.projectName() + "-ci-user";
         final IManagedPolicy denyChangingProtectedResourcesPolicy = ManagedPolicy.fromManagedPolicyName(this,
                 AccountCleanupStack.DENY_CHANGING_PROTECTED_RESOURCE_POLICY,
                 AccountCleanupStack.DENY_CHANGING_PROTECTED_RESOURCE_POLICY);
+
+        final User ciUser = defineUser(props, denyChangingProtectedResourcesPolicy);
+        CfnOutput.Builder.create(this, OUTPUT_CI_USER_NAME).value(ciUser.getUserName()).build();
+
+        if (props.createRole()) {
+            final Role ciRole = defineRole(props, ciUser, denyChangingProtectedResourcesPolicy);
+            CfnOutput.Builder.create(this, OUTPUT_CI_ROLE_ARN).value(ciRole.getRoleArn()).build();
+        }
+    }
+
+    private @NotNull User defineUser(final CiUserStackProps props,
+                                     final IManagedPolicy denyChangingProtectedResourcesPolicy) {
+        final String ciUserName = PROTECTED + props.projectName() + "-ci-user";
         final User ciUser = new User(this, ciUserName, UserProps.builder().userName(ciUserName).build());
         ciUser.addManagedPolicy(denyChangingProtectedResourcesPolicy);
         tagResource(ciUser);
-        final List<ManagedPolicy> policies = createPolicies(props);
+        final List<ManagedPolicy> policies = createPolicies(ciUserName, props.requiredPermissions());
         addPolicies(ciUser, policies);
-        CfnOutput.Builder.create(this, OUTPUT_CI_USER_NAME).value(ciUser.getUserName()).build();
+        return ciUser;
+    }
+
+    private @NotNull Role defineRole(final CiUserStackProps props, final User ciUser,
+                                     final IManagedPolicy denyChangingProtectedResourcesPolicy) {
+        final String ciRoleName = PROTECTED + props.projectName() + "-ci-role";
+        final RoleProps roleProps = RoleProps.builder()
+                .roleName(ciRoleName)
+                .assumedBy(ciUser)
+                .build();
+        final Role ciRole = new Role(this, ciRoleName, roleProps);
+        ciRole.addManagedPolicy(denyChangingProtectedResourcesPolicy);
+        tagResource(ciRole);
+        final List<ManagedPolicy> policies = createPolicies(ciRoleName, props.roleRequiredPermissions());
+        addPolicies(ciRole, policies);
+        return ciRole;
     }
 
     @NotNull
-    private List<ManagedPolicy> createPolicies(final CiUserStackProps props) {
+    private List<ManagedPolicy> createPolicies(final String resourceName, final List<PolicyDocument> requiredPermissions) {
         final List<ManagedPolicy> policies = new ArrayList<>();
         int counter = 1;
-        for (final PolicyDocument requiredPermission : props.requiredPermissions()) {
-            final String name = PROTECTED + props.projectName() + "-ci-user-policy-" + counter;
+        for (final PolicyDocument requiredPermission : requiredPermissions) {
+            final String name = resourceName + "-policy-" + counter;
             policies.add(createManagedPolicy(requiredPermission, name));
             counter++;
         }
         return policies;
     }
 
-    private void addPolicies(final User ciUser, final List<ManagedPolicy> policies) {
+    private void addPolicies(final IIdentity identity, final List<ManagedPolicy> policies) {
         for (final ManagedPolicy policy : policies) {
             tagResource(policy);
-            ciUser.addManagedPolicy(policy);
+            identity.addManagedPolicy(policy);
         }
     }
 
@@ -91,11 +121,25 @@ public class CiUserStack extends TaggedStack {
         String projectName();
 
         /**
+         * Whether a Role should be created by the stack.
+         *
+         * @return true if a role should be created, false (default) otherwise.
+         */
+        boolean createRole();
+
+        /**
          * Get AWS permissions required for this CI user.
          *
          * @return permissions
          */
         List<PolicyDocument> requiredPermissions();
+
+        /**
+         * Get AWS permissions required for the CI Role.
+         *
+         * @return permissions
+         */
+        List<PolicyDocument> roleRequiredPermissions();
     }
 
     /**
@@ -103,11 +147,23 @@ public class CiUserStack extends TaggedStack {
      */
     public static class DefaultCiUserStackProps implements CiUserStackProps {
         private final String projectName;
+        private final boolean createRole;
         private final List<PolicyDocument> requiredPermissions;
+        private final List<PolicyDocument> roleRequiredPermissions;
 
         private DefaultCiUserStackProps(final Builder builder) {
             this.projectName = builder.projectName;
+            this.createRole = builder.createRole;
             this.requiredPermissions = builder.requiredPermissions;
+            this.roleRequiredPermissions = builder.roleRequiredPermissions;
+            if (!roleRequiredPermissions.isEmpty() && !createRole) {
+                throw new IllegalArgumentException("Only when createRole is true roleRequiredPermissions can be set");
+            }
+        }
+
+        @Override
+        public boolean createRole() {
+            return createRole;
         }
 
         @Override
@@ -120,12 +176,19 @@ public class CiUserStack extends TaggedStack {
             return this.requiredPermissions;
         }
 
+        @Override
+        public List<PolicyDocument> roleRequiredPermissions() {
+            return roleRequiredPermissions;
+        }
+
         /**
          * Builder for {@link DefaultCiUserStackProps}.
          */
         public static class Builder {
             private final List<PolicyDocument> requiredPermissions = new ArrayList<>();
+            private final List<PolicyDocument> roleRequiredPermissions = new ArrayList<>();
             private String projectName;
+            private boolean createRole;
 
             /**
              * Set the project name.
@@ -135,6 +198,17 @@ public class CiUserStack extends TaggedStack {
              */
             public Builder projectName(final String projectName) {
                 this.projectName = projectName;
+                return this;
+            }
+
+            /**
+             * Set whether to create a role in the stack.
+             *
+             * @param createRole true to create a role, false (default) otherwise
+             * @return self for fluent programming
+             */
+            public Builder createRole(final boolean createRole) {
+                this.createRole = createRole;
                 return this;
             }
 
@@ -150,7 +224,19 @@ public class CiUserStack extends TaggedStack {
             }
 
             /**
-             * Build the @link CiUserStackProps}.
+             * Add permissions to the CI Role.
+             * Should only be used if {@code createRole} is set to true.
+             *
+             * @param roleRequiredPermissions AWS permissions
+             * @return self for fluent programming
+             */
+            public Builder addRoleRequiredPermissions(final PolicyDocument... roleRequiredPermissions) {
+                this.roleRequiredPermissions.addAll(Arrays.asList(roleRequiredPermissions));
+                return this;
+            }
+
+            /**
+             * Build the {@link CiUserStackProps}.
              *
              * @return built {@link CiUserStackProps}
              */
--- src/test/java/com/exasol/ciisolation/aws/ciuser/CiUserStackTest.java
+++ src/test/java/com/exasol/ciisolation/aws/ciuser/CiUserStackTest.java
@@ -1,20 +1,42 @@
 package com.exasol.ciisolation.aws.ciuser;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-import org.junit.jupiter.api.Test;
-
 import com.exasol.ciisolation.aws.PolicyReader;
-
+import org.junit.jupiter.api.Test;
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.assertions.Template;
+
+import java.util.Map;
+
+import static com.exasol.ciisolation.aws.ciuser.CiUserStack.OUTPUT_CI_ROLE_ARN;
+import static com.exasol.ciisolation.aws.ciuser.CiUserStack.OUTPUT_CI_USER_NAME;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 
 class CiUserStackTest {
+
     @Test
-    void synthStack() {
-        final App app = new App();
+    void testStack() {
         final PolicyReader policyReader = new PolicyReader();
-        new CiUserStack(app, CiUserStack.CiUserStackProps.builder().projectName("dummy-project")
+        final CiUserStack stack = new CiUserStack(new App(), CiUserStack.CiUserStackProps.builder().projectName("dummy-project")
                 .addRequiredPermissions(policyReader.readPolicyFromResources("test-permissions.json")).build());
-        assertDoesNotThrow(() -> app.synth());
+
+        final Template template = Template.fromStack(stack);
+        assertOutputs(template, OUTPUT_CI_USER_NAME);
+    }
+
+    @Test
+    void testStackWithRole() {
+        final PolicyReader policyReader = new PolicyReader();
+        final CiUserStack stack = new CiUserStack(new App(), CiUserStack.CiUserStackProps.builder().projectName("dummy-project")
+                .createRole(true)
+                .addRequiredPermissions(policyReader.readPolicyFromResources("test-permissions.json")).build());
+
+        final Template template = Template.fromStack(stack);
+        assertOutputs(template, OUTPUT_CI_USER_NAME, OUTPUT_CI_ROLE_ARN);
+    }
+
+    private void assertOutputs(final Template template, final String... outputNames) {
+        final Map<String, Map<String, Object>> outputs = template.findOutputs("*");
+        assertThat(outputs.keySet(), contains(outputNames));
     }
 }
